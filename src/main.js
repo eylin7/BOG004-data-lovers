40d7ed2d8a75246b01a30eeb876242facf485063
eylin7 <97687659+eylin7@users.noreply.github.com>
2022-02-09 11:27:33 -0500

    primer cambio

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { mostrarDatos } from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+let pagina1= document.getElementById("boton1");
+pagina1.addEventListener("click", mostrar);
+function mostrar() {
+  document.getElementById("bienvenida").style.display = "none";
+   document.getElementById("contenedor").style.display = "block";
+   document.getElementById("contenedor1").style.display = "none";
+   document.getElementById("imagen1").style.display = "none";
+   
+}
+
+mostrarDatos()
+  
