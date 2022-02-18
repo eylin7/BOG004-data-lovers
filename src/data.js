befd0ea1b234e167a421c964bc15c9324f6a9e4a
zulaymejia <zulaymejia06@gmail.com>
2022-02-18 11:20:37 -0500

    agregar segundo cambio

--- src/data.js
+++ src/data.js
@@ -4,3 +4,31 @@ export const filtrarTipos = (arregloDeData, seleccionado) => {
   );
   return arrayFiltrado;
 };
+
+export const ordenarPokemonZA =(poke)=>{
+  let ordenar= poke.sort((a= pokemon.name, b=pokemon.name)=>{
+if (b.name == a.name){
+  return 0;
+} 
+if (b.name < a.name){
+  return -1
+}
+return 1
+});
+return ordenar;
+};
+console.log(ordenarPokemonZA)
+
+export const ordenarPokemonAZ=(poke)=>{
+  let ordenar= poke.sort((a= pokemon.name, b=pokemon.name)=>{
+if (a.name == b.name){
+  return 0;
+} 
+if (a.name < b.name){
+  return -1
+}
+return 1
+});
+return ordenar;
+};
+console.log(ordenarPokemonAZ)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import data from "./data/pokemon/pokemon.js";
 import { filtrarTipos} from "./data.js";
+import { ordenarPokemonZA} from "./data.js";
+import { ordenarPokemonAZ} from "./data.js";
+
 let pokemones = data.pokemon;
 console.log(pokemones);
 //console.log(example, data);
@@ -14,13 +17,12 @@ function mostrar() {
 
 
 //Esta funcion muestra todos los pokemones en pantalla
-const mostrarDatos = (mostrarPokemones) => {
+/*const mostrarDatos = (mostrarPokemones) => {
   console.log(mostrarPokemones);
   let traertarjetas = document.getElementById("pokemones");
-  traertarjetas.innerHTML = "";
+  traertarjetas.innerHTML = "";//para que se limpie la pantalla cuando carga
   mostrarPokemones.forEach(function (pokemon, indice) {
     //console.log(pokemon, indice);
-
     const nombre = pokemon.name;
     const numeros = pokemon.num;
     let tarjeta = document.createElement("div");
@@ -36,13 +38,42 @@ const mostrarDatos = (mostrarPokemones) => {
     tarjeta.appendChild(p);
  
   });
+};*/
+
+const mostrarDatos= (descripcion)=>{ 
+  let traertarjetas = document.getElementById("pokemones");
+    traertarjetas.innerHTML = "";  
+  descripcion.forEach(function(pokemon, indice){
+    let tarjeta2 = document.createElement("div");
+    tarjeta2.setAttribute("class", "cuerpo-tarjetas");
+    if(descripcion == pokemones){
+      tarjeta2.innerHTML=`
+      <img class="img" src=${pokemon.img}>
+      <h3>${pokemon.name}</h3>
+      `
+    }else{
+
+      tarjeta2.innerHTML=`
+    <img class="img" src=${pokemon.img}>
+    <h3>${pokemon.name}</h3>
+    <p>${pokemon.about}</p>
+    `
+    }
+    
+    traertarjetas.appendChild(tarjeta2)
+  
+  })
+ 
 };
-//mostrarDatos(pokemones);
+
+
 
 let traerTipos= document.getElementById("tipos");
 traerTipos.addEventListener("change",()=> mostrarDatos(filtrarTipos(pokemones, traerTipos.value)));
 
+//let btnTodosLosPokemones= document.getElementById("boton1");
+//btnTodosLosPokemones.addEventListener("click",()=> mostrarDatos(pokemones));
+
+
 let btnTodosLosPokemones= document.getElementById("boton1");
-btnTodosLosPokemones.addEventListener("click",()=> mostrarDatos(pokemones));
-
-
+btnTodosLosPokemones.addEventListener("click",()=> mostrarDatos(ordenarPokemonZA(pokemones)));
